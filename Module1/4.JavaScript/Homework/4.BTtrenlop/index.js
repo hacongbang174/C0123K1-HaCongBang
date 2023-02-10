628634e6e1705dada622638ad7e794cfbac6c12b
Hà Công Bằng <hacongbang174@gmail.com>
2023-02-10 12:24:13 +0700

    BT condition2

--- Module1/4.JavaScript/Homework/4.BTtrenlop/index.js
+++ Module1/4.JavaScript/Homework/4.BTtrenlop/index.js
@@ -1,20 +1,28 @@
 function controlRight() {
-        document.getElementById("car").src = "right.jpg";
-        var element = document.getElementById("car").src;
-        element.style.left = parseInt(element.style.left) + 50 + "px";
+    document.getElementById("car").src = "right.jpg";
+    var element = document.getElementById("car");
+    element.style.width = 200 + "px";
+    element.style.height = 100 + "px";
+    element.style.left = parseInt(element.style.left) + 50 + "px";
     }
 function controlLeft() {
     document.getElementById("car").src = "left.jpg";
-    var element = document.getElementById("car").src;
+    var element = document.getElementById("car");
+    element.style.width = 200 + "px";
+    element.style.height = 100 + "px";
     element.style.left = parseInt(element.style.left) - 50 + "px";
 }
 function controlUp() {
     document.getElementById("car").src = "up.jpg";
-    var element = document.getElementById("car").src;
-    element.style.top = parseInt(element.style.top) + 50 + "px";
+    var element = document.getElementById("car");
+    element.style.width = 100 + "px";
+    element.style.height = 200 + "px";
+    element.style.top = parseInt(element.style.top) - 50 + "px";
 }
 function controlDown() {
     document.getElementById("car").src = "down.jpg";
-    var element = document.getElementById("car").src;
-    element.style.top = parseInt(element.style.top) - 50 + "px";
+    var element = document.getElementById("car");
+    element.style.width = 100 + "px";
+    element.style.height = 200 + "px";
+    element.style.top = parseInt(element.style.top) + 50 + "px";
 }
--- Module1/5.Conditional Structure/Homework/8.Condition2/index.js
+++ Module1/5.Conditional Structure/Homework/8.Condition2/index.js
@@ -59,5 +59,90 @@ function equation2() {
     let a = document.getElementById('ptb2a').value;
     let b = document.getElementById('ptb2b').value;
     let c = document.getElementById('ptb2c').value;
-    document.getElementById('result7').innerHTML = - b/a;
+    let delta = b*b - 4*a*c;
+    if (delta > 0) {
+        x1 = (- b + Math.sqrt(delta))/(2*a);
+        x2 = (- b - Math.sqrt(delta))/(2*a);
+        document.getElementById('result7').innerHTML = "Phương trình có hai nghiệm: x = " + x1 + " và x = " + x2;
+    }else if (delta == 0) {
+        x3 = -b/(2*a);
+        document.getElementById('result7').innerHTML = "Phương trình có nghiệm kép: x = " + x3;
+    }else document.getElementById('result7').innerHTML = "Phương trình vô nghiệm"
+}
+//Bài 8
+function yearOld() {
+    let age = Number(document.getElementById('yearOld').value);
+    switch (Number.isInteger(age)) {
+        case(true): 
+        if (0 < age && age < 120) {
+            document.getElementById('result8').innerHTML = "Tuổi của bạn là: " + age;
+            }else document.getElementById('result8').innerHTML = "Nhập lại tuổi của bạn";
+        break;
+        case(false):
+            document.getElementById('result8').innerHTML = "Nhập lại tuổi của bạn";
+        break;
+        default: "";
+    }
+}
+//Bài 9
+function checkEdgeTriangle() {
+    let a = parseInt(document.getElementById('edgeTri1').value);
+    let b = parseInt(document.getElementById('edgeTri2').value);
+    let c = parseInt(document.getElementById('edgeTri3').value);
+    if (a > 0 && b > 0 && c > 0) {
+        let x = a + b;
+        let y = b + c;
+        let z = a + c;
+        if (x > c) {
+            if (y > a) {
+                if (z > b) {
+                    document.getElementById('result9').innerHTML = "a, b, c là cạnh của một tam giác";
+                }else document.getElementById('result9').innerHTML = "a, b, c không là cạnh của một tam giác";
+            }
+        }
+    }else document.getElementById('result9').innerHTML = "a, b, c không là cạnh của một tam giác";
+}
+//Bài 10
+function moneyElectric() {
+    let num = parseInt(document.getElementById('numberElectric').value);
+    if (num > 400) {
+        document.getElementById('result10').innerHTML = Number(num)*2.323 + " VNĐ";
+    } else if (num > 300) {
+        document.getElementById('result10').innerHTML = Number(num)*2.231 + " VNĐ";
+    }else if (num > 200) {
+        document.getElementById('result10').innerHTML = Number(num)*1.971 + " VNĐ";
+    }else if (num > 100) {
+        document.getElementById('result10').innerHTML = Number(num)*1.590 + " VNĐ";
+    }else if (num > 50) {
+        document.getElementById('result10').innerHTML = Number(num)*1.459 + " VNĐ";
+    }else document.getElementById('result10').innerHTML = Number(num)*1.403 + " VNĐ";
+}
+//Bài 11
+function incomeTax() {
+    let income = parseInt(document.getElementById('income').value);
+    if (income > 80000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.35 - 9850000) + " VNĐ";
+    }else if (income > 80000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.3 - 5850000) + " VNĐ";
+    }else if (income > 52000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.25 - 3250000) + " VNĐ";
+    }else if (income > 18000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.2 - 1650000) + " VNĐ";
+    }else if (income > 10000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.15 - 750000) + " VNĐ";
+    }else if (income > 5000000) {
+        document.getElementById('result11').innerHTML = (Number(income)*0.1 - 250000) + " VNĐ";
+    }else document.getElementById('result11').innerHTML = Number(income)*0.05 + " VNĐ";
+}
+//Bài 12
+function interestBank() {
+    let money = parseInt(document.getElementById('money').value);
+    let month = parseInt(document.getElementById('month').value);
+    let interest = parseInt(document.getElementById('interestMonth').value);
+    let i = 0;
+    while (i < month) {
+    money = money + Number(money)*(Number(interest)/100);
+    i++;
+    }
+    document.getElementById('result12').innerHTML = money +" VNĐ";
 }
